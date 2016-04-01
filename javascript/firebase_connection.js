507f3572ff17be58806d20923989b7b9ba55296b
naomi saltis <naomisaltis@gmail.com>
2016-03-31 19:31:02 -0700

    added very basic functionality to read data from firebase and inject it into the html

---
+++ javascript/firebase_connection.js
@@ -0,0 +1,32 @@
+// main connection to firebase
+var bookstoreBase = new Firebase("https://blinding-torch-3304.firebaseio.com/");
+
+var bookListings = $('#bookListings');
+
+// loads the posts into the html
+bookstoreBase.child('posts').once("value", function(snapshot){
+	snapshot.forEach(function(postSnapshot){
+		var postSnap = postSnapshot.val();
+		
+		// test values, test html
+		// edit here to add more cetegories like price, date, condition, etc.
+		var book = postSnap.book;
+		var subject = postSnap.subject;
+		var category = postSnap.category;
+		var user = postSnap.user;
+		
+		// temporary html template
+		// again, edit as necessary
+		bookListings.append(
+			"<div class='post'>" +
+				"<p>"+book+"</p>"+
+				"<p>"+category+"</p>"+
+				"<p>"+subject+"</p>"+
+				"<p>"+user+"</p>"+
+			"</div>"
+		);
+	});
+}, function(error){
+	console.log(error);
+});
+
