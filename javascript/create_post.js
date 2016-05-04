cb47719cdd535b4cf5de6ff3c962fbf2bc519d57
naomi saltis <naomisaltis@gmail.com>
2016-05-04 12:46:42 -0700

    got form values into javascript

--- javascript/create_post.js
+++ javascript/create_post.js
@@ -2,13 +2,9 @@
 var bookstoreBase = new Firebase("https://blinding-torch-3304.firebaseio.com/");
 var bookstorePosts = bookstoreBase.child("posts");
 
-var title = $("#book_title");
-var author = $("#author");
-var isbn = $("#isbn");
+var submit = $("#submit");
 
-$("#create-post-button").click(function() {
-  newPost = { 'Title': title.text(), 'Author': author.text(), 'ISBN': isbn.text() };
-  console.log("Adding new post: ", newPost);
-  var newBookstorePost =  bookstorePosts.push(newPost);
-}); 
-   
+submit.click(function(){
+	var title = $("#book_title").val();
+	alert(title);
+})
