9be0272c1e4786d1256b0ab4db9219766345463b
Surinder Singh <surindersingh9495@gmail.com>
2016-05-07 14:10:27 -0700

    Data Is Live
    Got data onto firebase, I’m sure it’s not the best way, but after
    fooling around with it and not getting the results I wanted, I resorted
    to this.
    Modify or make simpler as you wish.

--- javascript/create_post.js
+++ javascript/create_post.js
@@ -2,12 +2,40 @@
 var bookstoreBase = new Firebase("https://blinding-torch-3304.firebaseio.com/");
 var bookstorePosts = bookstoreBase.child("posts");
 
-var title = $("#book_title");
-var author = $("#author");
-var isbn = $("#isbn");
+//var title = $("#book_title");
+//var author = $("#author");
+//var isbn = $("#isbn");
+//var subject = $("#subject");
+//var course = $("#subject_class");
+//var condition = $("#condition_type");
+//var comments = $("#condition_comment");
+
 
 $("#create-post-button").click(function() {
-  newPost = { 'Title': title.text(), 'Author': author.text(), 'ISBN': isbn.text() };
+  
+  var book_title = document.getElementById("book_title");
+  var title = book_title.value;
+  
+  var author1 = document.getElementById("author");
+  var author = author1.value;
+  
+  var isbn1 = document.getElementById("isbn");
+  var isbn = isbn1.value;
+  
+  var subject1 = document.getElementById("subject");
+  var subject = subject1.value;
+  
+  var subject_class = document.getElementById("subject_class");
+  var course = subject_class.value;
+  
+  var condition_type = document.getElementById("condition_type");
+  var condition = condition_type.value;
+  
+  var condition_comment = document.getElementById("condition_comment");
+  var comments = condition_comment.value;
+  
+  
+  newPost = { 'Title': title, 'Author': author, 'ISBN': isbn, 'Subject': subject, 'Course': course, 'Condition': condition, 'Comments': comments };
   console.log("Adding new post: ", newPost);
   var newBookstorePost =  bookstorePosts.push(newPost);
 }); 
