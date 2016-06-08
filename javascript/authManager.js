42836b626494ee9ac46b1de6569278006193aafd
Ian J Sikes <ianjsikes@gmail.com>
2016-06-07 23:22:21 -0700

    Created authManager.js to manage login states. Login modal to sign up w/ facebook and create a new account. :ok:

---
+++ javascript/authManager.js
@@ -0,0 +1,169 @@
+function AuthManager() {
+  if (!(this instanceof AuthManager)) {
+    return new AuthManager();
+  }
+
+  this.fbRef = new Firebase("https://blinding-torch-3304.firebaseio.com");
+  this.authData;
+  this.fbUser;
+  
+  this.debug = true;
+  
+  this.log = function(msg) {
+    if (this.debug)
+      console.log('[AuthManager] ' + msg);
+  };
+  this.error = function(msg) {
+    if (this.debug)
+      console.error('[AuthManager] Error: ' + msg);
+  };
+
+  this.states = {
+    // INITIALIZING: 'INITIALIZING',
+    LOGGED_OUT: 'LOGGED_OUT',
+    AUTHORIZING: 'AUTHORIZING',
+    VALIDATING: 'VALIDATING',
+    SIGNING_UP: 'SIGNING_UP',
+    LOGGED_IN: 'LOGGED_IN',
+  };
+
+  this.isValidState = function (state) {
+    for (var s in this.states) {
+      if (state === s) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  this.nextStates = {
+    // INITIALIZING: {
+    //   LOGGED_OUT: 'LOGGED_OUT',
+    // },
+    LOGGED_OUT: {
+      AUTHORIZING: 'AUTHORIZING',
+      VALIDATING: 'VALIDATING',
+      LOGGED_OUT: 'LOGGED_OUT'
+    },
+    AUTHORIZING: {
+      VALIDATING: 'VALIDATING',
+      LOGGED_OUT: 'LOGGED_OUT'
+    },
+    VALIDATING: {
+      SIGNING_UP: 'SIGNING_UP',
+      LOGGED_IN: 'LOGGED_IN',
+      LOGGED_OUT: 'LOGGED_OUT'
+    },
+    SIGNING_UP: {
+      LOGGED_IN: 'LOGGED_IN',
+      LOGGED_OUT: 'LOGGED_OUT'
+    },
+    LOGGED_IN: {
+      LOGGED_OUT: 'LOGGED_OUT'
+    }
+  };
+
+  // this.state = this.states.INITIALIZING;
+  this.state = this.states.LOGGED_OUT;
+
+  this.isValidTransitionState = function (nextState) {
+    var validStates = this.nextStates[this.state];
+    for (var s in validStates) {
+      if (nextState === s) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  this.transition = function (nextState) {
+    if (!this.isValidState(nextState) || !this.isValidTransitionState(nextState)) {
+      this.error('Attempted to transition to invalid state: ' + this.state + ' -> ' + nextState);
+      return false;
+    }
+
+    $(document).trigger('am:exitState', [this.state]);
+    $(document).trigger('am:stateChange', [this.state, nextState]);
+    this.state = nextState;
+    $(document).trigger('am:enterState', [this.state]);
+
+  };
+
+  this.fetchUser = function (cb) {
+    var uid = this.authData.uid;
+    this.fbRef.child('users').child(uid).once('value',
+      function (snapshot) {
+        this.fbUser = snapshot.val();
+        cb();
+      }.bind(this),
+      function (error) {
+        console.error(error);
+        cb();
+      }.bind(this));
+  };
+  
+  // Do any user validation here
+  this.isUserValid = function (user) {
+    return (user && user.name && user.contact);
+  };
+  
+  this.facebookAuth = function () {
+    this.fbRef.authWithOAuthPopup('facebook', function(err, authData) {
+      if (err) {
+        this.error('Facebook auth failed!');
+      }
+      else {
+        this.log('Facebook auth succeeded!');
+      }
+    }.bind(this));
+  }
+  
+  this.updateUser = function(newUser, cb) {
+    var uid = this.authData.uid;
+    this.log('Updating user');
+    this.fbRef.child('users').child(uid).update(newUser, function(err) {
+      if(!err) {
+        this.log('User update succeeded');
+      }
+      else {
+        this.error('User update failed');
+      }
+      cb(err);
+    }.bind(this));
+  }
+
+  this.fbRef.onAuth(function (authData) {
+    this.log('Firebase onAuth');
+    if (authData) {
+      this.authData = authData;
+      this.log('Firebase authData received');
+
+      this.transition(this.states.VALIDATING);
+
+      this.fetchUser(function () {
+        if (this.fbUser && this.isUserValid(this.fbUser)) {
+          this.log('User is valid, logging in...');
+          this.transition(this.states.LOGGED_IN);
+        }
+        else {
+          this.log('User is invalid, signing up...');
+          this.transition(this.states.SIGNING_UP);
+        }
+      }.bind(this));
+    }
+    else {
+      this.log('No Firebase authData');
+    }
+  }.bind(this));
+}
+
+$(document).on('am:stateChange', function (e, currentState, nextState) {
+  console.log('%c[AuthManager] State change: ' + currentState + ' -> ' + nextState, 'color: blue');
+});
+
+var authManager = new AuthManager();
+// if (authManager.state === authManager.states.INITIALIZING) {
+//   authManager.transition(authManager.states.LOGGED_OUT);
+// }
+
+
--- javascript/facebook_login.js
+++ javascript/facebook_login.js
@@ -1,37 +1,136 @@
-var ref = new Firebase("https://blinding-torch-3304.firebaseio.com");
-var usersRef = ref.child('users');
+if(!authManager) {
+  console.error('ERROR: Make sure authManager.js is included before this script!');
+}
 
- $("#logout").hide();
+// Get everything in a good initial state
+$(".logged-in").hide();
 
-//login
-$("#login").click(function(){
-  ref.authWithOAuthPopup("facebook", function(error, authData) {
-    if (error) {
-      console.log("Login Failed!", error);
-    } else {
-      console.log("Authenticated successfully with payload:", authData);
-    }
-  });
+$(document).on('am:enterState', function(event, state) {
+  if (state === authManager.states.LOGGED_OUT) {
+    $(".logged-out").show();
+    $("#username").text('');
+    
+    $(".logged-in").hide();
+    $(".authorizing").hide();
+    $(".signing-up").hide();
+    $("#signup-modal").modal('hide');
+    authManager.fbRef.unauth();
+  }
+  else if (state === authManager.states.LOGGED_IN) {
+    $(".logged-in").show();
+    $("#username").text(authManager.fbUser.name);
+    
+    $(".logged-out").hide();
+    $(".authorizing").hide();
+    $(".signing-up").hide();
+    $("#signup-modal").modal('hide');
+  }
+  else if (state === authManager.states.AUTHORIZING) {
+    $("#signup-modal").modal('show');
+    $(".authorizing").show();
+    
+    $(".logged-out").hide();
+    $(".logged-in").hide();
+    $(".signing-up").hide();
+  }
+  else if (state === authManager.states.VALIDATING) {
+    // $("#signup-modal").modal('show');
+    
+    $(".logged-out").hide();
+    $(".logged-in").hide();
+    $(".signing-up").hide();
+    $(".authorizing").hide();
+  }
+  else if (state === authManager.states.SIGNING_UP) {
+    $("#signup-modal").modal('show');
+    $(".signing-up").show();
+    fillSignupForm(authManager.fbUser);
+    
+    $(".logged-out").hide();
+    $(".logged-in").hide();
+    $(".authorizing").hide();
+  }
 });
 
-//nav options for logged in
-ref.onAuth(function(authData) {
-  if(authData) {
-    $("#login").hide();
-    $("#username").show();
-    $("#logout").show();  
-    $("#username").html(authData.facebook.displayName);
+$(document).trigger('am:enterState', [authManager.state]);
+
+// $(document).on('exitAuthState', function(event, state) {
+//   if (state === authManager.states.LOGGED_OUT) {
+//     $(".logged-out").hide();
+//   }
+//   else if (state === authManager.states.LOGGED_IN) {
+//     $(".logged-in").hide();
+//   }
+//   else if (state === authManager.states.AUTHORIZING) {
+//     $(".authorizing").hide();
+//   }
+//   else if (state === authManager.states.VALIDATING) {
     
-    usersRef.child(authData.uid).update({
-      name: authData.facebook.displayName
+//   }
+//   else if (state === authManager.states.SIGNING_UP) {
+    
+//   }
+// });
+
+
+
+ $("#signup, #login").click(function() {
+   authManager.transition(authManager.states.AUTHORIZING);
+ });
+ 
+ $("#facebook-signup").click(function() {
+   authManager.facebookAuth();
+ });
+ 
+ // Log the user out if they don't complete the sign up
+ $(".signup-modal-close").click(function() {
+   authManager.transition(authManager.states.LOGGED_OUT);
+ });
+
+function fillSignupForm(user) {
+  if (user.name) {
+    $("#signup-name").attr("placeholder", user.name);
+    $("#signup-name").val(user.name);
+    $("#signup-name").attr("readonly", true);
+  }
+  if (user.contact) {
+    if (user.contact.email) {
+      $("#signup-email").attr("placeholder", user.contact.email);
+      $("#signup-email").val(user.contact.email);
+      $("#signup-email").attr("readonly", true);
+    }
+    if (user.contact.phone) {
+      $("#signup-phone").attr("placeholder", user.contact.phone);
+      $("#signup-phone").val(user.contact.phone);
+      $("#signup-phone").attr("readonly", true);
+    }
+  }
+}
+
+$("#signup-modal-submit").click(function() {
+  var newUser = {
+    name: $("#signup-name").val(),
+    contact: {
+      email: $("#signup-email").val(),
+      phone: $("#signup-phone").val(),
+    }
+  }
+  
+  if (authManager.isUserValid(newUser)) {
+    authManager.updateUser(newUser, function(err) {
+      if (!err) {
+        authManager.fetchUser(function() {
+          authManager.transition(authManager.states.LOGGED_IN);
+        });
+      }
+      else {
+        
+      }
     });
   }
-}); 
+});
 
 //logout
 $("#logout").click(function() {
-  ref.unauth(); 
-  $("#login").show();
-  $("#username").hide();
-  $("#logout").hide();  
-}); 
+  authManager.transition(authManager.states.LOGGED_OUT);
+});
